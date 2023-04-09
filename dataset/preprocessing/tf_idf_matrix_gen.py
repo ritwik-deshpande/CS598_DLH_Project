ee10531975adffc7151bad5c76a4827153164397
ritwik-deshpande <ritwikdeshpande01@gmail.com>
2023-04-08 19:11:12 -0500

    Added Glove Embeddings

--- dataset/preprocessing/tf_idf_matrix_gen.py
+++ dataset/preprocessing/tf_idf_matrix_gen.py
@@ -1,33 +1,36 @@
 import numpy as np
 
-from data_preprocessing import DataPreprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 MAX_FEATURES = 600
 
+class TFIDFFeatureGeneration:
+    def __init__(self, df, disease_name):
+        self.df = df
+        self.disease_name = disease_name
 
-def tf_idf_matrix_gen(df, disease_name):
-    vectorizer = TfidfVectorizer(max_features=MAX_FEATURES)
+    def tf_idf_matrix_gen(self, ):
+        vectorizer = TfidfVectorizer(max_features=MAX_FEATURES)
 
-    docs = df['text'].values
-    tfidf_matrix = vectorizer.fit_transform(docs)
+        docs = self.df['text'].values
+        tfidf_matrix = vectorizer.fit_transform(docs)
 
-    words = vectorizer.get_feature_names_out()
+        words = vectorizer.get_feature_names_out()
 
-    X = tfidf_matrix.toarray()
-    Y = np.array(df[disease_name].values)
-    print(words, X.shape, Y.shape)
-    return X, Y, words
+        X = tfidf_matrix.toarray()
+        Y = np.array(self.df[self.disease_name].values)
+        print(words, X.shape, Y.shape)
+        return X, Y, words
 
-def main():
-    dataPreprocessing = DataPreprocessing('../train/train_data_textual.csv', 'Asthma')
-    preprocessed_df = dataPreprocessing.preprocess_data()
+# def main():
+#     dataPreprocessing = DataPreprocessing('../train/train_data_textual.csv', 'Asthma')
+#     preprocessed_df = dataPreprocessing.preprocess_data()
+#
+#     X, Y, words = TF_IDF_FeatureGeneration(preprocessed_df, 'Asthma').tf_idf_matrix_gen()
+#
+#     print(X.shape, Y.shape, words)
 
-    X, Y, words = tf_idf_matrix_gen(preprocessed_df, 'Asthma')
 
-    print(X.shape, Y.shape, words)
-
-
-
-if __name__ =='__main__':
-    main()
+#
+# if __name__ =='__main__':
+#     main()
--- dataset/preprocessing/word_embeddings_gen.py
+++ dataset/preprocessing/word_embeddings_gen.py
@@ -1,35 +1,40 @@
 import numpy as np
-
-from data_preprocessing import DataPreprocessing
 from gensim.models import Word2Vec
 
 VECTOR_SIZE = 10
 
-def word2vec_matrix_gen(df, disease_name):
-    sentences = df['text'].apply(lambda x: x.split(' ')).values
-    # print(sentences)
-    model = Word2Vec(sentences, vector_size=VECTOR_SIZE, window=5, min_count=1, workers=4)
 
-    X = []
+class Word2VecFeatureGeneration:
+    def __init__(self, df, disease_name):
+        self.df = df
+        self.disease_name = disease_name
+    def word2vec_matrix_gen(self):
+        sentences = self.df['text'].apply(lambda x: x.split(' ')).values
+        # print(sentences)
+        model = Word2Vec(sentences, vector_size=VECTOR_SIZE, window=5, min_count=1, workers=4)
 
-    for i, sentence in enumerate(sentences):
-        word_vectors = []
-        for j, word in enumerate(sentence):
-            word_vectors.append(model.wv.get_vector(word))
-        X.append(word_vectors)
-    X = np.array(X)
-    Y = np.array(df[disease_name].values)
-    words = model.wv.key_to_index.keys()
-    return X, Y, words
+        X = []
 
-def main():
-    dataPreprocessing = DataPreprocessing('../train/train_data_textual.csv', 'Asthma')
-    preprocessed_df = dataPreprocessing.preprocess_data()
-    print("Completed preprocessing")
-    X, Y, words = word2vec_matrix_gen(preprocessed_df, 'Asthma')
+        for i, sentence in enumerate(sentences):
+            word_vectors = []
+            for j, word in enumerate(sentence):
+                word_vectors.append(model.wv.get_vector(word))
+            X.append(word_vectors)
+        X = np.array(X)
+        Y = np.array(self.df[self.disease_name].values)
+        words = model.wv.key_to_index.keys()
+        print(words, X.shape, Y.shape)
 
-    print(X.shape, Y.shape, words)
+        return X, Y, words
+
+# def main():
+#     dataPreprocessing = DataPreprocessing('../train/train_data_textual.csv', 'Asthma')
+#     preprocessed_df = dataPreprocessing.preprocess_data()
+#     print("Completed preprocessing")
+#     X, Y, words = word2vec_matrix_gen(preprocessed_df, 'Asthma')
+#
+#     print(X.shape, Y.shape, words)
 
 
-if __name__ =='__main__':
-    main()
+# if __name__ =='__main__':
+#     main()
